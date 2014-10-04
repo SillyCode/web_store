4057c1307fd755371b04a879dd19db757bb93ce8
Mark Malin <drowqubes@gmail.com>
2014-10-04 14:07:30 +0300

    Change: added initial functions to find used method

--- utils/template.php
+++ utils/template.php
@@ -40,12 +40,51 @@ class translate_block {
 		$this->stack = $stack;
 	}
 	
-	public static function render($stack, $context) {
+	public static function render($stack, $content) {
 		$code_block = new translate_block($stack);
-		$regex = '/\{(\w+)\}(.*)[\t\n\r]\{\/\1\}/ms';
-		var_dump($context);
+// 		var_dump($code_block);
+		$regex = '/\{([^\s}]+)\s*([^\s}]+)\}\s*(.*)[\t\n\r]\{\/\1\s+\2\}/ms';
+		$callback = array($code_block, 'expand');
+// 		var_dump($callback);
+		return $code_block->resolve($code_block->find_method(preg_replace_callback($regex, $callback, $content)));
+		# 
+// 		var_dump($context);
 		
 // 		var_dump($stack);
+	}
+	
+	private function expand($match) {
+// 		
+	}
+	
+	private function resolve($body) {
+// 		var_dump($body);
+	}
+	
+	private function find_method($body) {
+		$regex = '/{([^\s}]+)\s*([^}]+)\}/';
+		$callback = array($this, 'exec_method');
+		var_dump($body);
+		return preg_replace_callback($regex, $callback, $body);
+	}
+	
+	private function exec_method($match) {
+// 		var_dump($match);
+		list(, $method, $params) = $match;
+// 		var_dump($method, $params);
+		$method = 'parse_' . $method;
+		if($method != __FUNCTION__ && method_exists($this, $method)) {
+			return $this->$method($params);
+		}
+		return null;
+	}
+	
+	private function parse_if() {
+		return "if";
+	}
+	
+	private function parse_loop() {
+		return "loop";
 	}
 }
 
