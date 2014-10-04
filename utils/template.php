be2cc5c48b2a27a536648abc7265d5d6ebf8ef28
Mark Malin <drowqubes@gmail.com>
2014-10-04 13:25:24 +0300

    Change: updated template file.
    Work in progress, yet

--- utils/template.php
+++ utils/template.php
@@ -2,7 +2,7 @@
 	
 class template {
 	private $__filename;
-// 	private $__properties = array();
+	private $__properties = array();
 	
 	public function __construct($filename) {
 		$this->__filename = $filename;
@@ -11,15 +11,41 @@ class template {
 	public function render() {
 		$filename = __DIR__ . '/../templates/' . $this->__filename;
 		if(file_exists($filename)) {
-			echo translate_block::render(@file_get_contents($filename));
+			echo translate_block::render(array($this->__properties), @file_get_contents($filename));
 		}
+	}
+	
+	public function __set($name, $value) {
+		$this->__properties[$name] = $value;
+	}
+	
+	public function __unset($name) {
+		unset($this->__properties[$name]);
+	}
+	
+	public function &__get($name) {
+		if (array_key_exists($name, $this->__properties)) { return $this->__properties[$name]; }
+		return $null;
+	}
+	
+	public function __isset($name) {
+		return isset($this->__properties[$name]);
 	}
 }
 
 class translate_block {
+	private $stack;
 	
-	function render($context) {
+	private function __construct($stack) { 
+		$this->stack = $stack;
+	}
+	
+	public static function render($stack, $context) {
+		$code_block = new translate_block($stack);
+		$regex = '/\{(\w+)\}(.*)[\t\n\r]\{\/\1\}/ms';
 		var_dump($context);
+		
+// 		var_dump($stack);
 	}
 }
 
