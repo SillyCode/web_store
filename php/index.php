01803c882874a56ed1f8506e816ed1ea56d6f90b
Mark Malin <drowqubes@gmail.com>
2014-10-10 09:52:48 +0300

    Change: added index.php file

--- php/index.php
+++ php/index.php
@@ -2,31 +2,26 @@
 
 require_once('../utils/template.php');
 
-// function  __autoload($class) {
-// 	var_dump($class);
-// }
-
 function tabs() {
 	$filenames = glob('*.php');
-	return array_map(function($filename) { 
-		$filename = str_replace('_',' ', pathinfo ($filename,  PATHINFO_FILENAME));
-		return array('url' => urlencode($filename), 'text' => ucwords($filename));
-	}, array_filter($filenames, function($filename) { 
+	return array_map(function($filename) {
+		$filename = str_replace(' ','_', pathinfo ($filename,  PATHINFO_FILENAME));
+		return array('url' => urlencode($filename) . '.php', 'text' => ucwords($filename));
+	}, array_filter($filenames, function($filename) {
 		return ($filename != 'index.php');
 	}));
 }
 
 function content() {
 	$requested_url = ($_SERVER['REQUEST_URI']);
-// 	var_dump($requested_url);
 	$class = str_replace('_','+', strtolower($requested_url));
-	var_dump($class);
+// 	var_dump($class);
 	ob_start();
 // 	$class::render();
 	return ob_get_clean();
 }
 
-$tpl = new template('test.tpl');
+$tpl = new template('index.tpl');
 $tpl->tabs = tabs();
 $tpl->content = content();
 $tpl->render();
